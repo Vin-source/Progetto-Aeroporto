3c8972296b7617c87ec3ec04939e4caa36fd9c0f
JosuePugliese <j.pugliese@studenti.unina.it>
2025-10-15 10:44:30 +0200

    Aggiunto Ospite.java nel package model

---
+++ src/main/java/model/Ospite.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Ospite {
+    protected String id = "0";
+    protected String email;
+    protected String password;
+
+
+    public Ospite(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
+
+
