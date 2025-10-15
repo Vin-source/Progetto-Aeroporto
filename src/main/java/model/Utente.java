860d8d98ffb1a5edf9500533a1a4dc02ba8c816e
JosuePugliese <j.pugliese@studenti.unina.it>
2025-10-15 11:15:02 +0200

    aggiunto Utente.java

---
+++ src/main/java/model/Utente.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Utente extends Ospite {
+    private ArrayList<Prenotazione> prenotazioni;
+    private String id;
+    private String email;
+    private String password;
+
+    public Utente(String id, String email, String password) {
+        super(email, password);
+        this.id = id;
+        this.prenotazioni = new ArrayList<Prenotazione>();
+    }
+
+
+    public ArrayList<Prenotazione> cercaPrenotazioni(String valore) {
+        ArrayList<Prenotazione> prenotazioniTrovate = new ArrayList<>();
+        prenotazioni.forEach(prenotazione -> {
+            if (prenotazione.passeggero.getNome().equals(valore) || prenotazione.volo.getIdVolo().equals(valore)) {
+                prenotazioniTrovate.add(prenotazione);
+            }
+        });
+        return prenotazioniTrovate;
+    }
+
+/*
+    public Prenotazione getPrenotazione(String id){
+        if(!prenotazioni.isEmpty()){
+            for(Prenotazione p : prenotazioni){
+                if(p.getIdPrenotazione().equals(id)){
+                    return p;
+                }
+            }
+        }
+        return null;
+    }
+*/
+
+    // -------------- OGGETTI ESTERNI ------------------
+
+    public ArrayList<Prenotazione> getPrenotazioni() { return this.prenotazioni; }
+    public void setPrenotazioni(ArrayList<Prenotazione> prenotazioni) { this.prenotazioni = prenotazioni; }
+
+}
