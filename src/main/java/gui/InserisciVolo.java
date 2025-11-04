7adfbe217c387a03261b16dec42ed3d3877fc1c1
Vin-source <vinxit@hotmail.it>
2025-11-04 19:43:13 +0100

    Aggiustato initListeners in InserisciVolo

--- src/main/java/gui/InserisciVolo.java
+++ src/main/java/gui/InserisciVolo.java
@@ -6,6 +6,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class InserisciVolo {
+    private JPanel inserisciVoloPanel;
+
+    private JTextField codiceVolo;
     private JTextField compagniaVolo;
     private JTextField origineVolo;
     private JTextField destinazioneVolo;
@@ -13,11 +16,12 @@ public class InserisciVolo {
     private JTextField orarioVolo;
     private JTextField ritardoVolo;
     private JComboBox<String> gateVolo;
+
     private JButton confermaButton;
     private JButton resetButton;
-    private JPanel inserisciVoloPanel;
-    private JTextField codiceVolo;
     private JButton annullaButton;
+
+
 
     public JFrame frame;
 //    private Controller controller;
@@ -29,6 +33,11 @@ public class InserisciVolo {
         frame.pack();
         frame.setVisible(true);
 
+      initListeners(frameChiamante);
+    }
+
+
+    public void initListeners(JFrame frameChiamante) {
         confermaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -54,61 +63,7 @@ public class InserisciVolo {
         });
     }
 
-    /*
-    public InserisciVolo(Controller controller, JFrame frameChiamante) {
-        this.controller = controller;
 
-        frame = new JFrame("Inserisci Volo");
-        frame.setContentPane(panelRoot);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-
-        confermaButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    String compagnia = compagniaVolo.getText();
-                    String origine = origineVolo.getText();
-                    String destinazione = destinazioneVolo.getText();
-                    String data = dataVolo.getText();
-                    String orario = orarioVolo.getText();
-                    String ritardo = ritardoVolo.getText();
-                    String gate = gateVolo.getSelectedItem().toString();
-
-                    if(compagnia.isEmpty() || origine.isEmpty() || destinazione.isEmpty() || data.isEmpty() || orario.isEmpty() || gate.isEmpty()) {
-                        JOptionPane.showMessageDialog(null, "Errore: Compila tutti i campi richiesti (escluso ritardo).");
-                        return;
-                    }
-
-                    Boolean inserimentoAccettato = controller.inserisciVolo(compagnia, origine, destinazione, data, orario, ritardo, gate);
-
-                    if(inserimentoAccettato){
-                        JOptionPane.showMessageDialog(null, "Volo inserito con successo.");
-                        resetFields();
-                        frameChiamante.setVisible(true);
-                        frame.setVisible(false);
-                        frame.dispose();
-                    }else{
-                        JOptionPane.showMessageDialog(null, "Errore nell'inserimento del volo!");
-                        resetFields();
-                    }
-
-
-
-                }catch(IllegalArgumentException ex){
-                    JOptionPane.showMessageDialog(null, ex.getMessage(), "Errore", JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        });
-
-        resetButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                resetFields();
-            }
-        });
-    }
-*/
     private void resetFields() {
         codiceVolo.setText("");
         compagniaVolo.setText("");
