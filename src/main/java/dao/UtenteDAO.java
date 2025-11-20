dd96ef2fbb1cbf1bf5d9d38caaac653664943fa7
Vin-source <vinxit@hotmail.it>
2025-11-20 20:29:59 +0100

    Iniziata implementazione database per effettuare la prenotazione

--- src/main/java/dao/UtenteDAO.java
+++ src/main/java/dao/UtenteDAO.java
@@ -9,4 +9,5 @@ import java.util.ArrayList;
 public interface UtenteDAO {
     public ArrayList<Volo> getVoliDB();
     public ArrayList<Prenotazione> getPrenotazioniDB(String email_utente);
+    public boolean effettuaPrenotazioneDB(String codiceVolo, String nome, String cognome, String cid, String posto);
 }
